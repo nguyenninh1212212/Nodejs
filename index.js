3b68647e526f7063844650511634f9db3383ce53
nguyenanninh1212212 <ninh56085@gmail.com>
2024-03-26 00:42:09 +0700

    change

--- index.js
+++ index.js
@@ -5,36 +5,32 @@ var morgan = require('morgan')
 var {engine} = require('express-handlebars');
 const path = require('path');
 const Login=require('./model/model')
-//_________________________________
+////
 app.use(morgan('combined'))
-//_________________________________
+////
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname,'views'));
-//_________________________________
+//
 app.use(express.static(path.join(__dirname,'/public')));
 
 //
 app.get('/', (req, res) => {
   res.render('Login')
 })
-//_________________________________
 app.get('/Signin', (req, res) => {
   res.render('Signin')
 })
-
 app.get('/home', async (req, res) => {
-  try {
-    const data = await Login.find({});
-    res.render('home', { data });
-    console.log(data);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).send('Internal Server Error');
+  try{
+  const data=(await Login.find({})).map(m=>m.toObject())
+  return res.render('home',{data})
   }
-});
-
-//_________________________________
+  catch (error) {
+    console.error(error);
+  }
+})
+////
 const db=require('./db/mg')
 db.connect()
 app.listen(port, () => {
--- model/model.js
+++ model/model.js
@@ -1,12 +1,12 @@
 const mongoose=require('mongoose')
 const Schema=mongoose.Schema
 
-const Login=new mongoose.Schema({
+const Login=new Schema({
      id:{type:String},
      user:{type:String},
      pass:{type:String}
 })
 
-module.exports=mongoose.model('Login',Login)
+module.exports=mongoose.model('login01',Login)
 
 
